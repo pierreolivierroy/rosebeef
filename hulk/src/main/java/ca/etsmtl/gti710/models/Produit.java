088962952cd2476ce5aef8f8f0bc6d64ab5a11a0
Pierre-Olivier Bourgeois <po@bourgeois.me>
2014-09-23 18:58:59 -0400

    Changement des packages

---
+++ hulk/src/main/java/ca/etsmtl/gti710/models/Produit.java
@@ -0,0 +1,15 @@
+package ca.etsmtl.gti710.models;
+
+public class Produit {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
