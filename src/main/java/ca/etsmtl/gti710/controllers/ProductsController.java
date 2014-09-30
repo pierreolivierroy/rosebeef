99fc7816f5f2bfb962ed188ef6b44d1832092258
Pierre-Olivier Bourgeois <po@bourgeois.me>
2014-09-30 18:53:12 -0400

    Connection

--- src/main/java/ca/etsmtl/gti710/controllers/ProductsController.java
+++ src/main/java/ca/etsmtl/gti710/controllers/ProductsController.java
@@ -1,11 +1,10 @@
 package ca.etsmtl.gti710.controllers;
 import java.util.ArrayList;
 
-import ca.etsmtl.gti710.openErp.Connection;
+import ca.etsmtl.gti710.models.Product;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ca.etsmtl.gti710.models.Produit;
 import ca.etsmtl.gti710.providers.IProvider;
 import ca.etsmtl.gti710.providers.StubProvider;
 
@@ -15,8 +14,7 @@ public class ProductsController {
 	IProvider provider = new StubProvider();
 	
 	@RequestMapping("/products")
-	public ArrayList<Produit> products() {
-
+	public ArrayList<Product> products() {
 		return provider.getProducts();
 	}
 	
