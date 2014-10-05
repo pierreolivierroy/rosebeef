e8eaa5d520772faf2afc60811c2be5609ad03720
rivardolivier <rivard.olivier@gmail.com>
2014-10-05 11:21:13 -0400

    Class for open ERP request with read exemple

--- src/main/java/ca/etsmtl/gti710/controllers/ProductsController.java
+++ src/main/java/ca/etsmtl/gti710/controllers/ProductsController.java
@@ -1,8 +1,10 @@
 package ca.etsmtl.gti710.controllers;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import ca.etsmtl.gti710.models.Product;
 
+import ca.etsmtl.gti710.openErp.ClientAPI;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,10 @@ public class ProductsController {
 	@RequestMapping("/products")
 	public ArrayList<Product> products() {
 		
-		Connection connect = (Connection) context.getBean("connection");
-		System.out.println(connect.connect());
+		ClientAPI client = (ClientAPI) context.getBean("client");
+        client.connect();
+        HashMap<Integer, Object> result = client.read();
+        System.out.println(result);
 		
 		return provider.getProducts();
 	}
---
+++ src/main/java/ca/etsmtl/gti710/openErp/ClientAPI.java
@@ -0,0 +1,122 @@
+package ca.etsmtl.gti710.openErp;
+
+
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import org.apache.xmlrpc.XmlRpcException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Vector;
+
+public class ClientAPI {
+
+    private String host = null;
+    private int port = 0;
+    private String login = null;
+    private String password = null;
+    private String database = null;
+    private int userId;
+
+    public ClientAPI() {
+
+    }
+
+    public void connect() {
+
+        Object[] params;
+        XmlRpcClient xmlrpcLogin = new XmlRpcClient();
+
+        XmlRpcClientConfigImpl xmlrpcConfigLogin = new XmlRpcClientConfigImpl();
+        xmlrpcConfigLogin.setEnabledForExtensions(true);
+        try {
+            xmlrpcConfigLogin.setServerURL(new URL("http", host, port, "/xmlrpc/common"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        xmlrpcLogin.setConfig(xmlrpcConfigLogin);
+
+        try {
+            //Connect
+            params = new Object[] {database, login, password};
+            Object id = xmlrpcLogin.execute("login", params);
+            if (id instanceof Integer) {
+                userId = (Integer)id;
+            }
+        }
+        catch (XmlRpcException e) {
+            //logger.warn("XmlException Error while logging to OpenERP: ",e);
+            System.out.println(e);
+        }
+        catch (Exception e)
+        {
+            //logger.warn("Error while logging to OpenERP: ",e);
+            System.out.println(e);
+        }
+    }
+
+    public HashMap<Integer, Object> read() {
+
+        XmlRpcClient xmlrpcLogin = new XmlRpcClient();
+
+        XmlRpcClientConfigImpl xmlrpcConfigLogin = new XmlRpcClientConfigImpl();
+        xmlrpcConfigLogin.setEnabledForExtensions(true);
+        try {
+            xmlrpcConfigLogin.setServerURL(new URL("http", host, port, "/xmlrpc/object"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        xmlrpcLogin.setConfig(xmlrpcConfigLogin);
+
+        try {
+
+
+            Object read[]=new Object[7];
+            read[0]=database;   //Nom de la base de donnée
+            read[1]=userId;   // ID de l'utilisateur
+            read[2]=password;  //mot de passe
+
+            //TODO Put those elements in the method parameters
+            read[3]="product.product";
+            read[4]="read";
+            read[5] = 7;
+            read[6] = null;
+
+            HashMap<Integer, Object> result = (HashMap<Integer, Object>)xmlrpcLogin.execute("execute", read);
+            return result;
+        }
+        catch (XmlRpcException e) {
+            //logger.warn("XmlException Error while logging to OpenERP: ",e);
+            System.out.println(e);
+        }
+        catch (Exception e)
+        {
+            //logger.warn("Error while logging to OpenERP: ",e);
+            System.out.println(e);
+        }
+
+        return null;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+}
