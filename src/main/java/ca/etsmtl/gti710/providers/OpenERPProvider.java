0a5b1aa0e0427d3b32ff5808d2d562bc357065f1
Pierre-Olivier Bourgeois <po@bourgeois.me>
2014-10-07 18:12:04 -0400

    Esti de marde

--- src/main/java/ca/etsmtl/gti710/providers/OpenERPProvider.java
+++ src/main/java/ca/etsmtl/gti710/providers/OpenERPProvider.java
@@ -2,7 +2,6 @@ package ca.etsmtl.gti710.providers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,27 +16,21 @@ public class OpenERPProvider implements IProvider {
 
 	@Override
 	public Product getProduct(int id) {
-	    client.connect();
 	    
-	    HashMap<String, Object> products = client.readProduct(id);
+		client.connect();
+	    
+	    HashMap<String, Object> openERPProduct = client.readProduct(id);
 
 	    Product p = new Product(id);
 	    
-	    for(Map.Entry<String, Object> entry : products.entrySet()) {
-	      	
-	    	if (entry.getKey().equals("name")) {
-	    		p.setName(entry.getValue().toString());
-	    	} else if (entry.getKey().equals("qty_available")) {
-	    		p.setQuantity((Double)entry.getValue());
-	    	} else if (entry.getKey().equals("description")) {
-	    		p.setDescription(entry.getValue().toString());
-	    	} else if (entry.getKey().equals("lst_price")) {
-	    		p.setPrice((Double)entry.getValue());
-	    	} else if (entry.getKey().equals("default_code")) {
-	    		p.setCode(entry.getValue().toString());
-	    	}
+	    p.setName(openERPProduct.get("name").toString());
+	    p.setQuantity((Double)openERPProduct.get("qty_available"));
+	    p.setPrice((Double)openERPProduct.get("lst_price"));
+	    boolean code = Boolean.parseBoolean(openERPProduct.get("default_code").toString());
+	    if (!code) {
+	    	p.setCode(openERPProduct.get("default_code").toString());
 	    }
-	    
+
 	    return p;
 	}
 
