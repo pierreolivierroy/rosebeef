db656514b6287a8b98b87df2d02919f7a8cfdbd6
dannyboyer <dannyboyer49@gmail.com>
2014-11-08 12:58:19 -0500

    amazon-api +AmazonController.java

---
+++ src/main/java/ca/etsmtl/gti710/controllers/AmazonController.java
@@ -0,0 +1,10 @@
+package ca.etsmtl.gti710.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AmazonController {
+
+}
