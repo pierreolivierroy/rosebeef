e8ee0a257419cec72fbd965805425286867ede25
pierreolivierroy <roy.pierre.o@gmail.com>
2014-11-12 22:41:19 -0500

    Access-Control-Allow-Headers pour POST /orders

--- src/main/java/ca/etsmtl/gti710/filter/CorsFilter.java
+++ src/main/java/ca/etsmtl/gti710/filter/CorsFilter.java
@@ -16,9 +16,9 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, HEAD");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Accept");
         chain.doFilter(req, res);
     }
 
